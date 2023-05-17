d1aa6d315228d592df5bab4c87897124dd3f9bfd
birchthrush <birchthrush@gmail.com>
2023-05-17 16:02:07 +0200

    Added base structure for BatchProcessingWindow

---
+++ EME Expression Map Editor/View/BatchProcessingWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EME_Expression_Map_Editor.View
+{
+    /// <summary>
+    /// Interaction logic for BatchProcessingWindow.xaml
+    /// </summary>
+    public partial class BatchProcessingWindow : Window
+    {
+        public BatchProcessingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EME Expression Map Editor/ViewModel/BatchProcessingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EME_Expression_Map_Editor.ViewModel
+{
+    class BatchProcessingViewModel : ViewModelBase
+    {
+        public override object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BatchProcessingViewModel()
+        {
+
+        }
+    }
+}
